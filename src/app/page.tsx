ad7e9d7d36226102dee952d794d3210278ec6899
Asad <asadwaleed93@gmail.com>
2025-05-15 12:19:49 +0500

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { PlateEditor } from "@/components/editor/plate-editor";
+
+export default function Home() {
+  return (
+    <div className="w-full" data-registry="plate">
+      <PlateEditor />
+    </div>
+  );
+}
